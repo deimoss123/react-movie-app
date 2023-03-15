802c9f9dc86cb0b2fe1320be6526ae74b34f4010
Alksnis <daniels.alksnis@accenture.com>
2023-03-15 10:37:24 +0200

    Add 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import Root from './routes/root';
 import LoginPage from './routes/login';
 import YourMoviesPage from './routes/your-movies';
 import ProfilePage from './routes/profile';
+import ErrorPage from './routes/error';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: '/login',
---
+++ src/routes/error.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../syles/errorPage.module.scss';
+
+export default function ErrorPage() {
+  return (
+    <div className={styles.errorPage}>
+      <h1>404</h1>
+      <h2>Page not found :(</h2>
+    </div>
+  );
+}
