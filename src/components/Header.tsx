f0c314d070f5ca010f4c8e5b3dcde9681106d34c
Alksnis <daniels.alksnis@accenture.com>
2023-03-16 15:17:52 +0200

    Logout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../auth';
 import styles from './Header.module.scss';
 
 export default function Header() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogoutBtn = () => {
+    logout().then(() => {
+      localStorage.removeItem('currentUser');
+      navigate('/login', { replace: true });
+    });
+  };
+
   return (
     <header className={styles.header}>
       <Link to="/home">Home</Link>
       <Link to="/your-movies">Your Movies</Link>
       <Link to="/profile">Profile</Link>
+      <button className={styles.logoutBtn} onClick={handleLogoutBtn}>
+        Log out
+      </button>
     </header>
   );
 }
