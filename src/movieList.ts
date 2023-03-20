9d826e7c2fb7fe6759697bb74f03729bc40fc001
Alksnis <daniels.alksnis@accenture.com>
2023-03-20 14:27:58 +0200

    Change movie count

--- src/movieList.ts
+++ src/movieList.ts
@@ -10,25 +10,25 @@ const defaultMovieList: AvailableMovie[] = [
     name: 'Shrek',
     genre: 'Comedy',
     rentalPrice: 2.77,
-    stock: 3,
+    stock: 5,
   },
   {
     name: 'Shrek 2',
     genre: 'Comedy',
     rentalPrice: 2.97,
-    stock: 1,
+    stock: 2,
   },
   {
     name: 'Shrek The Third',
     genre: 'Comedy',
     rentalPrice: 3.04,
-    stock: 2,
+    stock: 4,
   },
   {
     name: 'Shrek Forever After',
     genre: 'Comedy',
     rentalPrice: 3.17,
-    stock: 1,
+    stock: 3,
   },
 ];
 
